0217a0616609c6dce44fa2e9013133f06f9babc2
vadepu <vadepu@uncc.edu>
2017-12-20 19:04:50 -0500

    Hamster Run

---
+++ Hamster_run_Unity_source/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour 
+{
+
+	
+public Text scoreText;
+public Text hiScoreText;
+
+public float scoreCount;
+public float hiScoreCount;
+
+public float pointsPerSecond;
+public bool scoreIncreasing;
+// Use this for initialization
+	
+void Start () 
+{
+
+if(PlayerPrefs.HasKey("High-Score"))
+{
+hiScoreCount=PlayerPrefs.GetFloat("High-Score");
+	
+}
+}
+
+
+
+// Update is called once per frame
+	
+void Update () 
+{
+
+if(scoreIncreasing)
+{
+scoreCount+=pointsPerSecond*Time.deltaTime;	
+
+}
+if(scoreCount>hiScoreCount)
+{
+hiScoreCount=scoreCount;
+PlayerPrefs.SetFloat("High-Score",hiScoreCount);
+}
+	
+scoreText.text="Score: "+Mathf.Round(scoreCount);
+hiScoreText.text="High-Score: "+Mathf.Round(hiScoreCount);
+}
+
+public void AddScore(int pointsToAdd)
+{
+scoreCount+=pointsToAdd;
+}
+
+
+
+}
